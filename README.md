e0a0a92cf77a64d7dd2a4bfb1de9057a21f5b908
KID421 <suchild7@gmail.com>
2019-12-05 10:46:00 +0800

    完成多人輸入判斷

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Test_MultiplePlayerChoose
+ Unity 測試：單機多人玩家選擇
---
+++ UnityMultiplePlayerChoose/Assets/MultiplePlayerSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplePlayerSystem : MonoBehaviour
+{
+    public List<KeyCode> keys = new List<KeyCode>
+    {
+        KeyCode.W, KeyCode.I, KeyCode.Y
+    };
+
+    public List<KeyCode> controller = new List<KeyCode>(3);
+
+    public RectTransform[] player;
+
+    private void Update()
+    {
+        Check();
+        Move();
+    }
+
+    private void Check()
+    {
+        if (Input.anyKeyDown)
+        {
+            for (int i = 0; i < keys.Count; i++)
+            {
+                if (Input.inputString == keys[i].ToString().ToLower())
+                {
+                    controller.Add(keys[i]);
+                    keys.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.anyKeyDown)
+        {
+            for (int i = 0; i < controller.Count; i++)
+            { 
+                if (Input.inputString == controller[i].ToString().ToLower())
+                {
+                    player[i].anchoredPosition += Vector2.right * 250;
+                }
+            }
+        }
+    }
+}
